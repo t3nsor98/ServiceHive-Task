fff933eea21b9993d24d081d54f78d9e094bd2c1
Digbijaya Lenka <digbijayalenka@gmail.com>
2025-04-26 17:16:39 +0530

    Initialize image slider project with React, Vite, and Tailwind CSS
    - Created package.json with dependencies for React, Vite, and Tailwind CSS.
    - Added Vite logo SVG to public directory.
    - Set up Tailwind CSS in App.css and index.css.
    - Developed main App component with a header and date.
    - Included React logo SVG in assets.
    - Configured main entry point (main.jsx) for React application.
    - Established Vite configuration for React and Tailwind CSS integration.

---
+++ image_slider_task/slider/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return <>
+  <div> 
+  <h1>Service Hike Image Slider Task</h1>
+  <p>Date 26-04-2025</p>
+  
+  </div>
+
+  </>;
+}
+
+export default App;
